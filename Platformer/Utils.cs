c5472acaff8704060caddf214377214fb0e15559
user1 <user1@LAB122PC04>
2026-02-06 09:24:02 +0200

    some vector math

---
+++ Platformer/Utils.cs
@@ -0,0 +1,49 @@
+﻿namespace Platformer
+{
+    public struct Vector2
+    {
+        public double x;
+        public double y;
+
+        public Vector2(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+        public Vector2 Normalized()
+        {
+            return this.Divided(Length());
+        }
+        public Vector2 Multiplied(double num)
+        {
+            return new Vector2(num * x, num * y);
+        }
+        public Vector2 Divided(double num)
+        {
+            return new Vector2(num / x, num / y);
+        }
+    }
+    public static class InputManager
+    {
+        public enum InputType
+        {
+            None,
+            Up,
+            Down,
+            Left,
+            Right,
+            Back
+        }
+        public static List<InputType> GetInput()
+        {
+            throw new NotImplementedException();
+            
+
+        }
+    }
+
+}
